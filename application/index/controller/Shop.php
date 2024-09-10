644f8f980db347d08ad91222b36ee480115e3eab
Full Stack Developer <sniper62899@gmail.com>
2024-09-11 03:48:07 +0900

    updated

--- application/index/controller/Shop.php
+++ application/index/controller/Shop.php
@@ -3,13 +3,13 @@
 // +----------------------------------------------------------------------
 // | ThinkAdmin
 // +----------------------------------------------------------------------
-// | ��Ȩ���� 2014~2019 
+// | ��Ȩ���� 2014~2019
 // +----------------------------------------------------------------------
 
 // +----------------------------------------------------------------------
 
 // +----------------------------------------------------------------------
-// | 
+// |
 
 // +----------------------------------------------------------------------
 
@@ -34,9 +34,9 @@ class Shop extends Base
     {
         $this->banner = db('xy_banner')->select();
         //if($this->banner) $this->banner = explode('|',$this->banner);
-        $this->gundong = db('xy_index_msg')->where('id',8)->value('content');;;
-        $this->shoplist = db('xy_shop_goods_list')->where('is_tj',1)->limit(10)->select();
-        $this->assign('pic','/upload/qrcode/user/'.(session('user_id')%20).'/'.session('user_id').'-1.png');
+        $this->gundong = db('xy_index_msg')->where('id', 8)->value('content');
+        $this->shoplist = db('xy_shop_goods_list')->where('is_tj', 1)->limit(10)->select();
+        $this->assign('pic', '/upload/qrcode/user/' . (session('user_id') % 20) . '/' . session('user_id') . '-1.png');
         $this->cate = db('xy_shop_goods_cate')->order('id asc')->select();
 
         $this->lixibao = db('xy_lixibao_list')->order('id asc')->find();
@@ -44,71 +44,81 @@ class Shop extends Base
         return $this->fetch();
     }
 
-
-    public function cartlist() {
+    public function cartlist()
+    {
         $uid = session('user_id');
         if (!$uid) {
             $this->redirect('User/login');
         }
         $itemsPerPage = 10;
-        $cartlist = db('xy_goods_list')->where('status', 0)->paginate($itemsPerPage);
+        $cartlist = db('xy_goods_list')->where('status', 1)->paginate($itemsPerPage);    
+        // Check if the cartlist is empty
+        if ($cartlist->isEmpty()) {
+            $this->assign('noItemsMessage', 'Nothing is added to the cart');
+        }
         $this->assign('cartlist', $cartlist);
         $this->assign('pagination', $cartlist->render());
         $this->select = "cart";
         return $this->fetch();
     }
 
-
     public function goodslist()
     {
         $where = [];
-        if(input('cid/d',0))$where[] = ['cid','=',input('cid/d',0)];
-        if(input('name/s',''))$where[] = ['goods_name','like','%' . input('name/s','') . '%'];
+        if (input('cid/d', 0)) {
+            $where[] = ['cid', '=', input('cid/d', 0)];
+        }
+
+        if (input('name/s', '')) {
+            $where[] = ['goods_name', 'like', '%' . input('name/s', '') . '%'];
+        }
 
         $this->_query('xy_shop_goods_list')->where($where)->page();
         return $this->fetch();
     }
 
-
-
     public function orderlist()
     {
         $where = [];
-        if(input('cid/d',0))$where[] = ['cid','=',input('cid/d',0)];
-        if(input('name/s',''))$where[] = ['goods_name','like','%' . input('name/s','') . '%'];
+        if (input('cid/d', 0)) {
+            $where[] = ['cid', '=', input('cid/d', 0)];
+        }
 
-        //һ���
-        if(request()->isPost()) {
+        if (input('name/s', '')) {
+            $where[] = ['goods_name', 'like', '%' . input('name/s', '') . '%'];
+        }
+
+        if (request()->isPost()) {
             $uid = session('user_id');
-            $page = input('post.page/d',1);
-            $num = input('post.num/d',10);
-            $limit = ( (($page - 1) * $num) . ',' . $num );
-            $status = input('post.status/d',1);
+            $page = input('post.page/d', 1);
+            $num = input('post.num/d', 10);
+            $limit = ((($page - 1) * $num) . ',' . $num);
+            $status = input('post.status/d', 1);
 
             $data = db('xy_shop_order')
-                ->where('uid',session('user_id'))
-                ->where('status',$status)
+                ->where('uid', session('user_id'))
+                ->where('status', $status)
                 ->order('addtime desc')
                 ->limit($limit)
                 ->select();
 
             foreach ($data as &$datum) {
-                //$datum['endtime'] = date('Y/m/d H:i:s',$datum['endtime']);
-                $datum['addtime'] = date('Y/m/d H:i:s',$datum['addtime']);
+                $datum['addtime'] = date('Y/m/d H:i:s', $datum['addtime']);
             }
 
+            if (!$data) {
+                json(['code' => 1, 'info' => lang('')]);
+            }
 
-            if(!$data) json(['code'=>1,'info'=>lang('��������')]);
-            return json(['code'=>0,'info'=>lang('����ɹ�'),'data'=>$data]);
+            return json(['code' => 0, 'info' => lang(''), 'data' => $data]);
         }
         return $this->fetch();
     }
 
-
     public function detail()
     {
-        $id      = input('get.id/d',1);
-        $this->info = db('xy_shop_goods_list')->find($id);;
+        $id = input('get.id/d', 1);
+        $this->info = db('xy_shop_goods_list')->find($id);
 
         return $this->fetch();
     }
@@ -116,108 +126,105 @@ class Shop extends Base
     public function order_info()
     {
         $uid = session('user_id');
-            if(!$uid) $this->redirect('User/login'); 
-        $id      = input('get.id/d',1);
-        $this->endtime = date('Y/m/d H:i:s', time()+30*60);
-        $this->address = db('xy_member_address')->where('uid',$uid)->find();
-        $this->balance = db('xy_users')->where('id',$uid)->value('balance');
+        if (!$uid) {
+            $this->redirect('User/login');
+        }
+
+        $id = input('get.id/d', 1);
+        $this->endtime = date('Y/m/d H:i:s', time() + 30 * 60);
+        $this->address = db('xy_member_address')->where('uid', $uid)->find();
+        $this->balance = db('xy_users')->where('id', $uid)->value('balance');
         $this->goods = db('xy_shop_goods_list')->find($id);
 
         return $this->fetch();
     }
 
-
-
-     public function order_detail()
+    public function order_detail()
     {
         $uid = session('user_id');
-            if(!$uid) $this->redirect('User/login'); 
-        $id      = input('get.oid/s',1);
-        $this->endtime = date('Y/m/d H:i:s', time()+3*24*60*60);
-        $this->address = db('xy_member_address')->where('uid',$uid)->find();
-        $this->balance = db('xy_users')->where('id',$uid)->value('balance');
+        if (!$uid) {
+            $this->redirect('User/login');
+        }
 
-        $order = db('xy_shop_order')->where('id',$id)->find();
-//ar_dump($order);die;
+        $id = input('get.oid/s', 1);
+        $this->endtime = date('Y/m/d H:i:s', time() + 3 * 24 * 60 * 60);
+        $this->address = db('xy_member_address')->where('uid', $uid)->find();
+        $this->balance = db('xy_users')->where('id', $uid)->value('balance');
+
+        $order = db('xy_shop_order')->where('id', $id)->find();
 
         $this->goods = db('xy_shop_goods_list')->find($order['gid']);
         $this->order = $order;
 
-
         return $this->fetch();
     }
 
-
-    //���ɶ�����
-    function getSn($head='')
+    public function getSn($head = '')
     {
         @date_default_timezone_set("PRC");
         $order_id_main = date('YmdHis') . mt_rand(1000, 9999);
-        //Ψһ�������루YYMMDDHHIISSNNN��
-        $osn = $head.substr($order_id_main,2); //���ɶ�����
+        $osn = $head . substr($order_id_main, 2);
         return $osn;
     }
 
     public function do_order()
     {
-        $id = input('post.id','');
+        $id = input('post.id', '');
         $uid = session('user_id');
-            if(!$uid) $this->redirect('User/login'); 
-        $num = input('post.num',1);
+        if (!$uid) {
+            $this->redirect('User/login');
+        }
+
+        $num = input('post.num', 1);
         $goods = db('xy_shop_goods_list')->find($id);
         if (!$goods) {
-            return json(['code'=>1,'info'=>lang('��Ʒ�����쳣'),'data'=>[]]);
+            return json(['code' => 1, 'info' => lang('��Ʒ�����쳣'), 'data' => []]);
         }
 
         if ($num <= 0) {
-            return json(['code'=>1,'info'=>'you are sb','data'=>[]]);
+            return json(['code' => 1, 'info' => 'you are sb', 'data' => []]);
         }
 
-
-
-
-        if (!$num) return json(['code'=>1,'info'=>lang('�����쳣'),'data'=>[]]);
-        $balance = db('xy_users')->where('id',$uid)->value('balance');
-        if ( $balance < ($goods['goods_price']*$num) ) {
-            return json(['code'=>1,'info'=>lang('��������,���ȳ�ֵ'),'data'=>[]]);
+        if (!$num) {
+            return json(['code' => 1, 'info' => lang('�����쳣'), 'data' => []]);
         }
 
-        if( $goods['goods_price']*$num < 0 ) {
-            return json(['code'=>1,'info'=>'you are sb','data'=>[]]);
+        $balance = db('xy_users')->where('id', $uid)->value('balance');
+        if ($balance < ($goods['goods_price'] * $num)) {
+            return json(['code' => 1, 'info' => lang('��������,���ȳ�ֵ'), 'data' => []]);
+        }
+
+        if ($goods['goods_price'] * $num < 0) {
+            return json(['code' => 1, 'info' => 'you are sb', 'data' => []]);
         }
 
         $id1 = getSn('SP');
         $data = [
-            'id'        =>$id1,
-            'uid'       => $uid,
-            'gid'       => $id,
-            'price'       => $goods['goods_price'],
-            'num'      => $num,
-            'price2'      => $goods['goods_price']*$num,
-            'status'   => 1,
-            'addtime'   => time()
+            'id' => $id1,
+            'uid' => $uid,
+            'gid' => $id,
+            'price' => $goods['goods_price'],
+            'num' => $num,
+            'price2' => $goods['goods_price'] * $num,
+            'status' => 1,
+            'addtime' => time(),
         ];
-        $res = Db::name('xy_users')->where('id',$uid)->setDec('balance',$goods['goods_price']*$num);
-
-        //
-        $res1 = Db::name('xy_balance_log')->insert([
-            //��¼��Ӷ��Ϣ
-            'uid'       => $uid,
-            'oid'       => $id1,
-            'num'       => $goods['goods_price']*$num,
-            'type'      => 11,
-            'addtime'   => time()
+        $res = db('xy_users')->where('id', $uid)->setDec('balance', $goods['goods_price'] * $num);
+        $res1 = db('xy_balance_log')->insert([
+            'uid' => $uid,
+            'oid' => $id1,
+            'num' => $goods['goods_price'] * $num,
+            'type' => 11,
+            'addtime' => time(),
         ]);
 
         $res = db('xy_shop_order')->insert($data);
-        if($res)
-            return json(['code'=>0,'info'=>lang('�����ɹ�')]);
-        else
-            return json(['code'=>1,'info'=>lang('����ʧ��')]);
+        if ($res) {
+            return json(['code' => 0, 'info' => lang('')]);
+        } else {
+            return json(['code' => 1, 'info' => lang('')]);
+        }
 
     }
-
-
-
 
 }
--- runtime/temp/00039ce14be7d36f26b5922120c1dfe1.php
+++ runtime/temp/00039ce14be7d36f26b5922120c1dfe1.php
@@ -1,6 +1,6 @@
-<?php /*a:2:{s:67:"F:\xampp\htdocs\think_php\application\index\view\shop\cartlist.html";i:1725976821;s:66:"F:\xampp\htdocs\think_php\application\index\view\public\floor.html";i:1725953038;}*/ ?>
+<?php /*a:2:{s:67:"F:\xampp\htdocs\think_php\application\index\view\shop\cartlist.html";i:1725987528;s:66:"F:\xampp\htdocs\think_php\application\index\view\public\floor.html";i:1725953038;}*/ ?>
 <!DOCTYPE html><html><head><meta charset="utf-8" /><meta name="viewport"
-        content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1" /><meta name="apple-mobile-web-app-capable" content="yes" /><meta content="telephone=yes" name="format-detection" /><meta name="apple-mobile-web-app-status-bar-style" content="white" /><meta name="x5-fullscreen" content="true" /><meta name="apple-touch-fullscreen" content="yes" /><title><?php echo lang('首页'); ?></title><link rel="stylesheet" href="/statics/css/global.css" /><link rel="stylesheet" href="/web/css/cartlist.css" /><link rel="stylesheet" href="/static_new/css/public.css"><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"><script charset="utf-8" src="/static_new/js/jquery.min.js"></script><script charset="utf-8" src="/static_new/js/dialog.min.js"></script><script charset="utf-8" src="/static_new/js/common.js"></script></head><body><div id="app"><div class="catlist-container"><div class="cartlist-header"><div class="cartlist-header-text">Shopping cart</div><button class="cartlist-header-btn"><i class="material-icons-outlined color-FF5035">delete</i></button></div><div class="cartlist-body"><?php if(is_array($cartlist) || $cartlist instanceof \think\Collection || $cartlist instanceof \think\Paginator): $i = 0; $__LIST__ = $cartlist;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$item): $mod = ($i % 2 );++$i;?><div class="cartlist-item"><i class="material-icons-outlined color-FF5035">radio_button_checked</i><?php if($item['goods_pic']): ?><img class="cartlist-item-img" src="<?php echo htmlentities($item['goods_pic']); ?>" /><?php else: ?><div class="cartlist-item-img"></div><?php endif; ?><div style="margin-left: 10px; width: 100%;"><div class="cartlist-item-title"><?php echo htmlentities($item['goods_name']); ?></div><div class="cartlist-item-price"><div>￥<?php echo htmlentities($item['goods_price']); ?></div><button class="cartlist-item-btn bg-FF5035">Go to Result</button></div></div></div><?php endforeach; endif; else: echo "" ;endif; ?><!-- Display pagination links --><div><?php echo $pagination; ?></div></div></div><link href="/web/css/footer.css" rel="stylesheet" /><div class="footer"><a href="/index/index/home"><div class="<?php if($select == 'home'): ?>footer-active-item<?php endif; ?> footer-item"><i class="<?php if($select == 'home'): ?>material-icons<?php else: ?>material-icons-outlined<?php endif; ?>">home</i><div><?php echo lang('Home page'); ?></div></div></a><a href="/index/shop/cartlist"><div class="<?php if($select == 'cart'): ?>footer-active-item<?php endif; ?> footer-item"><i class="<?php if($select == 'cart'): ?>material-icons<?php else: ?>material-icons-outlined<?php endif; ?>">shopping_cart</i><div><?php echo lang('Shopping cart'); ?></div></div></a><div class="<?php if($select == 'order'): ?>footer-active-item<?php endif; ?> footer-item"><i class="<?php if($select == 'order'): ?>material-icons<?php else: ?>material-icons-outlined<?php endif; ?>">list_alt</i><div><?php echo lang('Order for goods'); ?></div></div><div class="<?php if($select == 'my'): ?>footer-active-item<?php endif; ?> footer-item"><i class="<?php if($select == 'my'): ?>material-icons<?php else: ?>material-icons-outlined<?php endif; ?>">person</i><div><?php echo lang('Mine'); ?></div></div></div><script>
+        content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1" /><meta name="apple-mobile-web-app-capable" content="yes" /><meta content="telephone=yes" name="format-detection" /><meta name="apple-mobile-web-app-status-bar-style" content="white" /><meta name="x5-fullscreen" content="true" /><meta name="apple-touch-fullscreen" content="yes" /><title><?php echo lang('首页'); ?></title><link rel="stylesheet" href="/statics/css/global.css" /><link rel="stylesheet" href="/web/css/cartlist.css" /><link rel="stylesheet" href="/static_new/css/public.css"><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"><script charset="utf-8" src="/static_new/js/jquery.min.js"></script><script charset="utf-8" src="/static_new/js/dialog.min.js"></script><script charset="utf-8" src="/static_new/js/common.js"></script></head><body><div id="app"><div class="catlist-container"><div class="cartlist-header"><div class="cartlist-header-text">Shopping cart</div><button class="cartlist-header-btn"><i class="material-icons-outlined color-FF5035">delete</i></button></div><div class="cartlist-body"><?php if(isset($noItemsMessage)): ?><div class="cartlist-no-item"><div class="cartlist-no-item-text"><?php echo htmlentities($noItemsMessage); ?></div><button class="cartlist-no-item-btn">Continue shopping</button></div><?php else: if(is_array($cartlist) || $cartlist instanceof \think\Collection || $cartlist instanceof \think\Paginator): $i = 0; $__LIST__ = $cartlist;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$item): $mod = ($i % 2 );++$i;?><div class="cartlist-item" onclick="itemActive(<?php echo htmlentities($item['id']); ?>, <?php echo htmlentities($item['addtime']); ?>)"><div class="cartlist-item-timecount display-none" id="cartlist-item-timecount-<?php echo htmlentities($item['id']); ?>"></div><i class="material-icons-outlined cartlist-item-radio-icon" id="material-icons-outlined-<?php echo htmlentities($item['id']); ?>">radio_button_checked</i><?php if($item['goods_pic']): ?><img class="cartlist-item-img" src="<?php echo htmlentities($item['goods_pic']); ?>" /><?php else: ?><div class="cartlist-item-img"></div><?php endif; ?><div style="margin-left: 10px; width: 100%;"><div class="cartlist-item-title"><?php echo htmlentities($item['goods_name']); ?></div><div class="cartlist-item-price"><div>￥<?php echo htmlentities($item['goods_price']); ?></div><button class="cartlist-item-btn" id="cartlist-item-btn-<?php echo htmlentities($item['id']); ?>">Go to Result</button></div></div></div><?php endforeach; endif; else: echo "" ;endif; ?><?php endif; ?><!-- Display pagination links --><div><?php echo $pagination; ?></div></div></div><link href="/web/css/footer.css" rel="stylesheet" /><div class="footer"><a href="/index/index/home"><div class="<?php if($select == 'home'): ?>footer-active-item<?php endif; ?> footer-item"><i class="<?php if($select == 'home'): ?>material-icons<?php else: ?>material-icons-outlined<?php endif; ?>">home</i><div><?php echo lang('Home page'); ?></div></div></a><a href="/index/shop/cartlist"><div class="<?php if($select == 'cart'): ?>footer-active-item<?php endif; ?> footer-item"><i class="<?php if($select == 'cart'): ?>material-icons<?php else: ?>material-icons-outlined<?php endif; ?>">shopping_cart</i><div><?php echo lang('Shopping cart'); ?></div></div></a><div class="<?php if($select == 'order'): ?>footer-active-item<?php endif; ?> footer-item"><i class="<?php if($select == 'order'): ?>material-icons<?php else: ?>material-icons-outlined<?php endif; ?>">list_alt</i><div><?php echo lang('Order for goods'); ?></div></div><div class="<?php if($select == 'my'): ?>footer-active-item<?php endif; ?> footer-item"><i class="<?php if($select == 'my'): ?>material-icons<?php else: ?>material-icons-outlined<?php endif; ?>">person</i><div><?php echo lang('Mine'); ?></div></div></div><script>
     function updateactivetime() {
         var url = "<?php echo url('user/updateactivetime'); ?>";
         var isopenoline = 1;
@@ -23,4 +23,44 @@
     }
     updateactivetime();
     setInterval(updateactivetime, 1000 * 30);
-</script></div></body><script type="application/javascript"></script></html>
+</script></div></body><script type="application/javascript">    var selectedId = 0;
+    var timerInterval;
+    var countdownTime = 0;
+
+    function updateTimer(id) {
+        const hours = Math.floor(countdownTime / 3600);
+        const minutes = Math.floor((countdownTime % 3600) / 60);
+        const seconds = Math.floor(countdownTime % 60);
+
+        $('#cartlist-item-timecount-' + id).text(`Distance from end ${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`);
+
+        if (countdownTime <= 0) {
+            clearInterval(timerInterval);
+        } else {
+            countdownTime--;
+        }
+    }
+
+    function itemActive(id, addtime) {
+        $('.cartlist-item-btn').removeClass('bg-FF5035');
+        $('.cartlist-item-radio-icon').removeClass('color-FF5035');
+        $('#cartlist-item-btn-' + id).addClass('bg-FF5035');
+        $('#material-icons-outlined-' + id).addClass('color-FF5035');
+        $('.cartlist-item-timecount').removeClass('display-block');
+        $('.cartlist-item-timecount').addClass('display-none');
+        $('#cartlist-item-timecount-' + id).removeClass('display-none');
+        $('#cartlist-item-timecount-' + id).addClass('display-block');
+        selectedId = id;
+        countdownTime = 12 * 60 * 60 + Number(addtime); // Reset to 12 hours
+        countdownTime = countdownTime - (Date.now() / 1000)
+        if (countdownTime > 0) {
+            clearInterval(timerInterval); // Clear any existing interval
+            timerInterval = setInterval(() => updateTimer(id), 1000); // Start a new interval
+            updateTimer(id); // Initial call to display the timer immediately
+        }
+    }
+
+    // setInterval(() => {
+    //     window.location.reload()
+    // }, 10000)
+</script></html>
